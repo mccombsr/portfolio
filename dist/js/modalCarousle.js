87b638436eeecbcad77014d90ea79a3ac42c060f
Ryan McCombs <mccombs008@yahoo.com>
2018-12-11 09:43:16 -0700

    added group project and made some touch ups

--- dist/js/modalCarousle.js
+++ dist/js/modalCarousle.js
@@ -2,16 +2,42 @@ var slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n) {
-    showDivs(slideIndex += n);
+  showDivs((slideIndex += n));
 }
 
 function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("images");
-    if (n > x.length) {slideIndex = 1} 
-    if (n < 1) {slideIndex = x.length} ;
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none"; 
-    }
-    x[slideIndex-1].style.display = "block"; 
+  var i;
+  var x = document.getElementsByClassName("images");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
+
+var slideIndex2 = 1;
+showDivs(slideIndex2);
+
+function plusDivs2(n) {
+  showDivs2((slideIndex2 += n));
+}
+
+function showDivs2(n) {
+  var i;
+  var x = document.getElementsByClassName("images2");
+  if (n > x.length) {
+    slideIndex2 = 1;
+  }
+  if (n < 1) {
+    slideIndex2 = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex2 - 1].style.display = "block";
 }
